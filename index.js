b17b75662db7611f98b814e19e8c96aed41cc384
mdhasankhan-123 <hasankha1023@gmail.com>
2023-11-11 11:55:58 +0600

    (PSS): MongoDB added on the project

--- index.js
+++ index.js
@@ -1,10 +1,66 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport')
 const cors = require('cors');
 const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors())
+
+// mailgun setup
+const auth = {
+    auth: {
+      api_key: '6d8d428c-d11cf93b',
+      domain: 'sandbox6d236b137a78458099e9181053cdb718.mailgun.org'
+    }
+  }
+
+// userName: Portfolio-server
+// password: B7lFkueqbh8D5IVy
+
+
+// mongodb start
+const uri = "mongodb+srv://Portfolio-server:B7lFkueqbh8D5IVy@cluster0.kgqmoa1.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.log);
+
+
+
 
 
 // first gating
