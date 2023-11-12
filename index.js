7ae3d3c7146704547ebcd8fddc07095ebdbe4956
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2023-11-13 01:52:17 +0600

    (PSS): Add email route and configaration email send system

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const cors = require('cors');
 const nodemailer = require('nodemailer');
 const mg = require('nodemailer-mailgun-transport')
-const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors())
+app.use(express.json())
 
 // mailgun setup
 const auth = {
     auth: {
-      api_key: '6d8d428c-d11cf93b',
-      domain: 'sandbox6d236b137a78458099e9181053cdb718.mailgun.org'
+      api_key: `${process.env.MG_API_KEY}`,
+      domain: `${process.env.MG_DOMAIN}`
     }
   }
+
+  
+const transport = nodemailer.createTransport(mg(auth));
 
 
 // mongodb start
@@ -45,6 +49,31 @@ async function run() {
         const result = await allProjects.find().toArray();
         res.send(result)
     })
+
+    // Email
+    app.post('/email', (req, res) => {
+      const mailInfo = req.body;
+      const {email, message, name, subject} = mailInfo;
+      // console.log(email, message, name, subject)
+
+      transport.sendMail({
+          from: `${email}`,
+          to: 'mdhasankha.wd@gmail.com',
+          subject: `${subject}`,
+          html: `
+        <h1 style={text-align: center}>${subject}</h1>
+          <p>${message}</p>
+          `
+      }, (err, info) => {
+          if (err) {
+              console.log(`Error: ${err}`);
+          }
+          else {
+              res.send(info);
+          }
+      });
+  })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
